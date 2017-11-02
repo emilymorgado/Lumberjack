3646997e37749045e8a0aa6709e06ea363bed994
Emily Morgado <emilyannemorgado@gmail.com>
2017-11-01 20:41:52 -0700

    initial commit and sync solution

---
+++ solution/sync-sorted-merge.js
@@ -0,0 +1,18 @@
+'use strict'
+var arraySort = require('array-sort');
+
+module.exports = (logSources, printer) => {
+    const strippedLogs = []
+
+    for(let log in logSources) {
+        strippedLogs.push(logSources[log].last)
+    }
+
+    const sortedLogs = arraySort(strippedLogs, 'date')
+
+    for(let log in sortedLogs) {
+        let logEntry = sortedLogs[log]
+        printer.print(logEntry)
+        printer.done()        
+    }
+}
